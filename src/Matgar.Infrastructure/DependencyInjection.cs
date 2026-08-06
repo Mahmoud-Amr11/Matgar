bf2cca82a4cdd09c60804883b26e8410d4ecdde5
Mahmoud Amr Mahmoud <mahmoud.amr.dev@gmail.com>
2026-08-06 18:57:02 +0300

    feat(infrastructure): add database context and dependency injection setup
    * Add ApplicationDbContext to the Infrastructure layer
    
    * Add Infrastructure dependency injection configuration
    
    * Update project dependencies and references
    
    * Update API launch settings
    
    * Update .gitignore configuration

---
+++ src/Matgar.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Matgar.Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Matgar.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            AddPersistence(services, configuration);
+            return services;
+        }
+        private static IServiceCollection AddPersistence(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+            return services;
+        }
+
+    }
+}
---
+++ src/Matgar.Infrastructure/Persistence/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Matgar.Infrastructure.Persistence.Contexts
+{
+    public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
